d9c52e7bc1ead23dee808919074c1cfb7125fbeb
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-10-30 13:52:15 -0300

    Initial Commit

---
+++ NadaADeclarar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NadaADeclarar
+//
+//  Created by Gabriel Vicentin Negro on 30/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NadaADeclarar/NadaADeclararApp.swift
@@ -0,0 +1,17 @@
+//
+//  NadaADeclararApp.swift
+//  NadaADeclarar
+//
+//  Created by Gabriel Vicentin Negro on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NadaADeclararApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
