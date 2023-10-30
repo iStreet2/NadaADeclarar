fc12120fb4f4ae76cd3cbf3cc9ef6bc083c8db94
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-10-30 16:14:06 -0300

    Adicionar HomeView e outras coisas

---
+++ NadaADeclarar/Components/SubtituloView.swift
@@ -0,0 +1,24 @@
+//
+//  SubtituloView.swift
+//  NadaADeclarar
+//
+//  Created by Gabriel Vicentin Negro on 30/10/23.
+//
+
+import SwiftUI
+
+struct SubtituloView: View {
+    
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.custom("CabinetGrotesk-Regular", size: 20))
+            .foregroundStyle(Color("LightGrayCustom"))
+            .opacity(0.6)
+    }
+}
+
+#Preview {
+    SubtituloView(text: "Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.")
+}
