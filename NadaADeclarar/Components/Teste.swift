4fdf9c785319ff9bf4c19d76166dea0e19555d0f
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-10-30 13:58:45 -0300

    Testar

---
+++ NadaADeclarar/Components/Teste.swift
@@ -0,0 +1,18 @@
+//
+//  Teste.swift
+//  NadaADeclarar
+//
+//  Created by Gabriel Vicentin Negro on 30/10/23.
+//
+
+import SwiftUI
+
+struct Teste: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Teste()
+}
